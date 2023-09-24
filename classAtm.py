915f33420b33da92da6639ad90e86e22159fbc44
Harshit <harshitjain5june@gmail.com>
2023-09-24 13:43:30 +0530

    Encapsulation concepts added

--- classAtm.py
+++ classAtm.py
@@ -6,8 +6,12 @@ class Atm:
     
     def __init__(self) -> None:
         """Only gets called when a new obj of class is created, these are called magic methods in python which gets called on their own at their moment, they don't need to be called using objects."""
-        self.pin = ""
-        self.balance = 0
+
+        #These are instance variables (pin,balance) as they are different for every object , also the variables which you don't want accessible to objects so that they can directly alter it should be kept private, hence we use __ before the variable name
+
+        #Note that nothing is python is truly private you can still access the private variables using _Atm__balance format and alter it (Atm is class name)
+        self.__pin = ""
+        self.__balance = 0
         self.choice()
 
     def choice(self):
@@ -23,15 +27,26 @@ class Atm:
         else:
             print("Invalid choice")
 
+    # For programmers we can add get and set variable methods so they can modify code additionally we can add our login in methods so the code doesn't crash, it is better than directly accessing the variables and altering it.
+    def get_pin(self):
+        print(self.__pin)
+
+    def set_pin(self, new_pin):
+        if type(new_pin) == str:
+            self.__pin = new_pin
+            print(self.__pin)
+        else:
+            print("Not allowed")
+
     def change_pin(self):
-        self.pin = input("Enter your pin")
+        self.__pin = input("Enter your pin")
         print("Pin changed successfully!")
 
     def deposit_money(self):
         temp = input("Enter your pin")
-        if(temp == self.pin):
+        if(temp == self.__pin):
             amount = int(input("Enter amount to deposit"))
-            self.balance += amount
+            self.__balance += amount
             print("Amount added successfully!")
         else:
             print("Wrong pin entered, try again.")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from classAtm import Atm;
+from classAtm import Atm
 from classFraction import Fraction
-# sbi=Atm()
-x = Fraction(5,6)
+sbi = Atm()  # Here Sbi is reference variable which is pointing to Object Atm()
+x = Fraction(5, 6)
 print(x)
